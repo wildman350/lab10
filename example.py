1ff8e65dba53637fa9e075420250472036e42048
Ian Edgeley <ian@iedgeley.com>
2014-10-19 11:05:34 -0500

    Fixed comment in example.py

--- example.py
+++ example.py
@@ -6,13 +6,19 @@
 
 from Tkinter import *
 root = Tk()
+
 # Create the canvas widget
 drawpad = Canvas(root, width=800,height=600, background='brown')
 drawpad.grid(row=0, column=1)
-# create_oval(x,y,width,height,fill color)
+
+# create_oval(top left x,top left y, bottom right x, bottom right y,fill color)
 oval = drawpad.create_oval(10, 50, 100, 100, fill='green')
+
+
 #create_square(top left x,top left y, bottom right x, bottom right y, fill color)
 square = drawpad.create_rectangle(200,200,250,250, fill='red')
 #create_line(top left x,top left y, bottom right x, bottom right y, fill color)
 line = drawpad.create_line(0, 0, 200, 100)
+
+
 root.mainloop()
